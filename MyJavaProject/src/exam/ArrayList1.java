3ad0da220f3157e27e66b0c9f74e049e04716491
durisalasarath <durisalasarath8@gmail.com>
2023-04-15 00:41:56 -0500

    updated code

---
+++ MyJavaProject/src/exam/ArrayList1.java
@@ -0,0 +1,10 @@
+package exam;
+import java.util.ArrayList;
+public class ArrayList1 {
+    public static void main(String[] args) {
+        ArrayList<String> sar = new ArrayList<String>();
+         sar.add("sarath");
+        sar.add("sai");
+        System.out.println(sar);
+    }
+}
---
+++ MyJavaProject/src/exam/Str.java
@@ -0,0 +1,8 @@
+package exam;
+
+public class Str {
+    public static void main(String[] args) {
+        String s="sarath";
+        System.out.println(s.substring(s.length()-2,s.length()));
+    }
+}
