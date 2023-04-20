30f975e71364fb9d4d9d141433adbc96cba3e957
durisalasarath <durisalasarath8@gmail.com>
2023-04-19 20:58:18 -0500

    updated code abstract

---
+++ MyJavaProject/src/staticJ/Employee.java
@@ -0,0 +1,11 @@
+package staticJ;
+public class Employee {
+
+    public void printDetails() {
+        System.out.println("Printing Employee Details");
+    }
+
+    public static void printEmpDept() {
+        System.out.println("Printing emp Dept");
+    }
+}
---
+++ MyJavaProject/src/staticJ/MainClass.java
@@ -0,0 +1,9 @@
+package staticJ;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        Employee.printEmpDept();
+    }
+}
