6a7916a34ca65b2e5cbe81d4113c8720b8dc4532
durisalasarath <durisalasarath8@gmail.com>
2023-04-23 23:04:13 -0500

    spring boot project

---
+++ Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Spring/boot/tutorial/controller/DepartmentController.java
@@ -0,0 +1,50 @@
+package com.dailycodebuffer.Spring.boot.tutorial.controller;
+
+import com.dailycodebuffer.Spring.boot.tutorial.entity.Department;
+import com.dailycodebuffer.Spring.boot.tutorial.service.DepartmentService;
+import com.dailycodebuffer.Spring.boot.tutorial.service.DepartmentServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.swing.plaf.PanelUI;
+import java.util.List;
+
+@RestController
+public class DepartmentController {
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    @PostMapping("/departments")
+    public Department saveDepartment(@RequestBody Department department){
+        return departmentService.saveDepartment(department);
+    }
+
+    @GetMapping("/departments")
+    public List<Department> fetchDepartmentList(){
+        return departmentService.fetchDepartmentList();
+    }
+
+    @GetMapping("/departments/{id}")
+    public Department fetchDepartmentById(@PathVariable("id") Long departmentId){
+        return departmentService.fetchDepartmentById(departmentId);
+    }
+
+    @DeleteMapping("/departments/{id}")
+    public String deleteDepartmentById(@PathVariable("id") Long departmentId){
+        departmentService.deleteDepartmentById(departmentId);
+        return "Department deleted successfully!!";
+    }
+
+    @PutMapping("/departments/{id}")
+    public Department updateDepartment(@PathVariable("id") Long departmentId,
+                                       @RequestBody Department department){
+        return departmentService.updateDepartment(departmentId,department);
+
+    }
+
+    @GetMapping("/departments/name/{name}")
+    public Department fetchDepartmentByName(@PathVariable("name") String departmentName){
+        return departmentService.fetchDepartmentByName(departmentName);
+    }
+}
